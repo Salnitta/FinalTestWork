8585ed8f228398dd081b7c39ecea10be11d3ca2c
Tatyana Salnikova <tatyana.saln@mail.ru>
2022-07-29 01:10:42 +0300

    Program: method FillArray

--- Program.cs
+++ Program.cs
@@ -3,3 +3,12 @@
     return new string[size];
 }
 
+void FillArray(string[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Please, enter string №{i + 1} of the input array:");
+        array[i] = Console.ReadLine();
+    } 
+    Console.WriteLine();
+}
